a01b2438fc945d917025ff45a773b586506e8a08
Jakub <qbajas@gmail.com>
2022-12-02 14:03:58 +0100

    adapt RSI data points

--- src/current.py
+++ src/current.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         data = bt.feeds.YahooFinanceCSVData(
             dataname="../resources/tickers/" + ticker + ".csv",
         # Do not pass values before this date
-        fromdate=datetime.date.today() - datetime.timedelta(days=365))
+        fromdate=datetime.date.today() - datetime.timedelta(days=100))
         # Do not pass values before this date
         # todate=datetime.datetime(2015, 12, 31))
 
