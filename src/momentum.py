b223f0128f40cfb4712d7d97156e1b793c46825f
Jakub <qbajas@gmail.com>
2023-03-31 20:09:53 +0200

    momentum

---
+++ src/momentum.py
@@ -0,0 +1,149 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import locale
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+
+# Import the backtrader platform
+import backtrader as bt
+
+import pprint
+
+# Create a Stratey
+from backtrader.analyzers import PeriodStats, AnnualReturn, Returns, SharpeRatio, TimeDrawDown
+
+day = 0
+
+# source:
+# https://systemtrader.pl/prosta-dlugoterminowa-strategia-dla-rynku-akcji-i-obligacji/#logika-strategii
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.setsizer(bt.sizers.AllInSizer())
+
+    def next(self):
+        global day
+        day += 1
+
+        if day > 250 and day % 20 == 0:
+            self.log("cash %d" % (self.sizer.broker.getcash()))
+
+            if self.datas[0][0] / self.datas[0][-250] < self.datas[3][0] / self.datas[3][-250]:
+                if not self.broker.getposition(self.datas[2]):
+                    self.close(data=self.datas[0])
+                    self.close(data=self.datas[1])
+                    self.buy(data=self.datas[2], size=self.getsizing(self.datas[2]) * 0.95)
+
+            elif self.datas[0][0] / self.datas[0][-250] > self.datas[1][0] / self.datas[1][-250]:
+                if not self.broker.getposition(self.datas[0]):
+                    self.close(data=self.datas[1])
+                    self.close(data=self.datas[2])
+                    self.buy(data=self.datas[0], size=self.getsizing(self.datas[0]) * 0.95)
+            else:
+                if not self.broker.getposition(self.datas[1]):
+                    self.close(data=self.datas[0])
+                    self.close(data=self.datas[2])
+                    self.buy(data=self.datas[1], size=self.getsizing(self.datas[1]) * 0.95)
+
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    ' BUY EXECUTED at {}, cost {}, com {}'.format(order.executed.price,
+                                                                  order.executed.value,
+                                                                  order.executed.comm)
+                )
+                self.buyprice = order.executed.price
+                self.buycom = order.executed.comm
+                self.buyStock = False
+            else:
+                self.log(
+                    ' SELL EXECUTED at price {}, cost {}, com {}'.format(order.executed.price,
+                                                                         order.executed.value,
+                                                                         order.executed.comm)
+                )
+            self.bar_executed = len(self)
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('WARNING: Order Canceled/Margin/Rejected')
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+        self.log('  OPERATION PROFIT, GROSS {}, NET{}'.format(trade.pnl,
+                                                              trade.pnlcomm))
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    cerebro.addanalyzer(SharpeRatio)
+    cerebro.addanalyzer(TimeDrawDown)
+    cerebro.addanalyzer(PeriodStats)
+    cerebro.addanalyzer(AnnualReturn)
+    cerebro.addanalyzer(Returns)
+
+    tickers = [
+        "SPY",
+        "ACWX",
+        "AGG",
+        "BIL",
+    ]
+
+    datas = []
+
+    for ticker in tickers:
+        # Create a Data Feed
+        data = bt.feeds.YahooFinanceCSVData(
+            dataname="../resources/tickers/" + ticker + ".csv",
+            fromdate=bt.datetime.datetime(2010, 11, 1)
+            # todate=datetime.datetime(2017, 7, 20)
+        )
+
+        data.start()
+
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(data)
+
+        datas.append(data)
+
+    # Set our desired cash start
+    cashstart = 100000.0
+    cerebro.broker.setcash(cashstart)
+    cerebro.broker.setcommission(leverage=100, commission=0.000035)  # 0.0035% of the operation value
+
+
+# Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    run = cerebro.run()
+
+    # cerebro.plot()
+
+    locale.setlocale(locale.LC_ALL, 'en_US')
+
+    # Print out the final result
+    print(locale.format_string("Final Portfolio Value: %d", cerebro.broker.getvalue(), grouping=True))
+    print(locale.format_string("Annualized return: %f percent",
+                               100 * (((cerebro.broker.getvalue() / cashstart) ** (1 / ((day-250) / 252))) - 1),
+                               grouping=True))
+
+    pp = pprint.PrettyPrinter(width=41, compact=True)
+    pp.pprint(run[0].analyzers[0].get_analysis())
+    pp.pprint(run[0].analyzers[1].get_analysis())
+    pp.pprint(run[0].analyzers[3].get_analysis())
+    pp.pprint(run[0].analyzers[4].get_analysis())
+
+    cerebro.plot()
